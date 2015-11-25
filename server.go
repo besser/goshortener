213eb4660700a471a2cc088347565d20a7f2eceb
Roberto Besser <rmbesser@gmail.com>
2015-11-25 02:39:54 -0200

    More implementation.

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ import (
 	"github.com/besser/goshortener/url"
 )
 
+type Headers map [ string ] string
+
 var (
   	port 	int
 	urlBase	string
@@ -23,5 +25,20 @@ func main() {
 	http.HandleFunc("/api/short", Shortener)
 	http.HandleFunc("/r/", Redirect)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func Shortener(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		answerWith(w, http.StatusMethodNotAllowed, Headers { "Allow": "POST" })
+		return
+	}
+}
+
+func answerWith(w http.ResponseWriter, status int, headers Headers) {
+	for k, v := range headers {
+		w.Header().Set(k, v)
+	}
+	
+	w.WriteHeader(status)
 }
